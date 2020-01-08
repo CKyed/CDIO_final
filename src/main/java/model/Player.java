b18c19fb148e5419ff2ef64ee2adb088231c3d1f
Idkirsch <s195483@student.dtu.dk>
2020-01-08 10:48:09 +0100

    Player attributes

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,4 +1,13 @@
 package model;
 
 public class Player {
+    //the player needs attributes such as name, account and inJail,
+    private String name;
+    private Account account = new Account();
+    private boolean inJail = false;
+
+
+
+
+
 }
