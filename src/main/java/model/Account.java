7818a1d9372ab590fab55b6e68270d645cfde3a8
Marie Seindal <marie@seindal.dk>
2020-01-08 11:32:33 +0100

    Account klassen oprettet

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Account {
+
+    int balance;
+
+    public int withdraw(int amount){
+        return balance-amount;
+    }
+
+    public void deposit(int amount){
+        balance+=amount;
+    }
+
 }
