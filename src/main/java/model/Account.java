043776e6781649c407cbe42a1eea73fbc183eeea
peterrevsbech <peter.revsbech@gmail.com>
2020-01-08 13:27:47 +0100

    Er ved at opdatere fieldAttributes.txt

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,4 +1,20 @@
 package model;
 
 public class Account {
+    private int balance;
+
+
+    public Account(){
+        this.balance = 30000;
+    }
+
+    public void withdraw(int amount){
+        this.balance-= amount;
+    }
+
+    public void deposit(int amount){
+        this.balance+= amount;
+    }
+
+
 }
