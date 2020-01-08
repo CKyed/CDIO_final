050f3c20e48695856832675e1dca54cee062971e
peterrevsbech <peter.revsbech@gmail.com>
2020-01-08 11:01:34 +0100

    Har lavet Die, DiceCup og DiceController.

--- src/main/java/controller/DiceController.java
+++ src/main/java/controller/DiceController.java
@@ -1,4 +1,48 @@
 package controller;
 
+import model.DiceCup;
+
 public class DiceController {
+    private DiceCup diceCup;
+    private int[] faceValues;
+    private boolean sameValue =false;
+
+
+    public DiceController(){
+        this.diceCup = new DiceCup(2,6);
+    }
+
+    public void roll() {
+        //Rolls and updates the diceNumber and the sameValue variables
+        this.diceCup.rollDice();
+        this.faceValues = new int[diceCup.getDiceNumber()];
+        for (int i = 0; i<diceCup.getDiceNumber();i++){
+            this.faceValues[i] = diceCup.getDice()[i].getFaceValue();
+        }
+        this.sameValue = true;
+        int counter =0;
+        while (sameValue==true){
+            if(counter==diceCup.getDiceNumber()-1){
+                break;
+            }
+
+            if (faceValues[counter] != faceValues[counter+1]){
+                this.sameValue=false;
+            }
+
+            counter++;
+        }
+
+
+        for (int i =0; i<diceCup.getDiceNumber();i++){
+
+
+
+        }
+
+    }
+
+
+
+
 }
--- src/main/java/model/DiceCup.java
+++ src/main/java/model/DiceCup.java
@@ -1,4 +1,30 @@
 package model;
 
 public class DiceCup {
+    private int diceNumber = 1;
+    private int dieSum = 0;
+    private Die[] dice;
+
+    public DiceCup(int diceNumber, int diceSideNumber) {
+        this.diceNumber = diceNumber;
+        dice = new Die[diceNumber];
+        for (int i = 0; i < diceNumber; i++) {
+            dice[i] = new Die(diceSideNumber);
+        }
+    }
+
+    public void rollDice() {
+        int sum = 0;
+        for (int i = 0; i < diceNumber; i++) {
+            dice[i].roll();
+        }
+    }
+
+    public int getDiceNumber() {
+        return diceNumber;
+    }
+
+    public Die[] getDice() {
+        return dice;
+    }
 }
