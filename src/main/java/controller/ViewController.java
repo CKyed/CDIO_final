d76cf46096ddbad6e0533607a5b308f4794c79ac
Marie Seindal <marie@seindal.dk>
2020-01-09 09:29:06 +0100

    Tjek for ens navne, ikke testet

--- src/main/java/controller/ViewController.java
+++ src/main/java/controller/ViewController.java
@@ -63,13 +63,21 @@ public class ViewController {
 
         String[] playerNames = new String[numberOfPlayers];
 
-        for (int i =0;i<numberOfPlayers;i++){
+        for (int i =0;i<numberOfPlayers;i++) {
             playerNames[i] = gui.getUserString("");
 
-            while (playerNames[i].equals("")||playerNames[i].equals(" ")){
+            while (playerNames[i].equals("") || playerNames[i].equals(" ")) {
                 System.out.println("Indtast nyt navn"); //TODO GUI-meddelese
                 playerNames[i] = gui.getUserString("");
             }
+
+            for (int j = 0; j < numberOfPlayers; j++) {
+
+                if (playerNames[i].equals(playerNames[j]) && i != j)
+                playerNames[i] = gui.getUserString("");
+
+            }
+
 
         }
 
