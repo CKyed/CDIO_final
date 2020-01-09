5b354ea780104f96363fa95f3bdadb515277308a
dat4 <s195469@student.dtu.dk>
2020-01-09 13:06:56 +0100

    Viewcontroller er klar med Boardinitiering revert problemer overstået

--- src/main/java/model/Fields/OwnableFile/Brewery.java
+++ src/main/java/model/Fields/OwnableFile/Brewery.java
@@ -3,6 +3,8 @@ package model.Fields.OwnableFile;
 import model.Fields.Ownable;
 
 public class Brewery extends Ownable {
+    private String type = "brew";
+    private int group = 14;
     public Brewery(String state) {
         super(state);
     }
@@ -10,5 +12,15 @@ public class Brewery extends Ownable {
     @Override
     public void setRent() {
 
+    }
+
+    @Override
+    public int getGroup() {
+        return this.group;
+    }
+
+    @Override
+    public String getType() {
+        return this.type;
     }
 }
--- src/main/java/model/Fields/OwnableFile/Ferry.java
+++ src/main/java/model/Fields/OwnableFile/Ferry.java
@@ -3,6 +3,8 @@ package model.Fields.OwnableFile;
 import model.Fields.Ownable;
 
 public class Ferry extends Ownable {
+    private String type = "ferry";
+    private int group = 13;
     private int rentLevel;
 
     public Ferry(String state) {
@@ -12,5 +14,15 @@ public class Ferry extends Ownable {
     @Override
     public void setRent() {
 
+    }
+
+    @Override
+    public int getGroup() {
+        return this.group;
+    }
+
+    @Override
+    public String getType() {
+        return this.type;
     }
 }
