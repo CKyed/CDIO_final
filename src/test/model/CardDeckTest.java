884f705915b0f344695ae70495894e1b05b9e270
Peter <peter.revsbech@gmail.com>
2020-01-15 15:33:37 +0100

    Bedre version a jUnit importeret

--- src/test/model/CardDeckTest.java
+++ src/test/model/CardDeckTest.java
@@ -5,15 +5,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardDeckTest {
-
+public String[] testCardTexts = {"test","test","test","test","test","test","test","test","test","test"};
     @Test
     void swap() {
-        
+        assertTrue(true);
 
     }
 
-    @Test
-    void shuffle() {
+     @Test
+    void shuffleTest() {
+        //Tests that cards are randomly shuffled
+        CardDeck deck = new CardDeck(testCardTexts);
+        int[] occurences = new int[testCardTexts.length];
+
+        for (int i = 0; i < 1000 ; i++) {
+            //Shuffles deck
+            deck.shuffle();
+
+            //Draws upper card and gets id - increments occurence
+            occurences[deck.draw().getId()]++;
+        }
+
+        for (int i = 0; i < testCardTexts.length; i++) {
+            assertTrue(occurences[i]<120 && occurences[i]> 80);
+        }
+
     }
 
     @Test
