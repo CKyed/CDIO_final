97a8ca6353327967326243dc1ecf652b3af76164
Peter <peter.revsbech@gmail.com>
2020-01-15 15:15:53 +0100

    Oprettet mapper til tests

---
+++ src/test/controller/PlayerControllerTest.java
@@ -0,0 +1,28 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerControllerTest {
+
+    @Test
+    void addMoneyToPlayer() {
+    }
+
+    @Test
+    void takeMoneyFromPlayer() {
+    }
+
+    @Test
+    void safeTransferToBank() {
+    }
+
+    @Test
+    void safeTransferToPlayer() {
+    }
+
+    @Test
+    void tryToPayDebt() {
+    }
+}
---
+++ src/test/model/CardDeckTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardDeckTest {
+
+    @Test
+    void swap() {
+        
+
+    }
+
+    @Test
+    void shuffle() {
+    }
+
+    @Test
+    void draw() {
+    }
+}
