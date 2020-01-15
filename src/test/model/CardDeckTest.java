e51fc170e1a1856b1dbfcd54821afce0512a03a3
Peter <peter.revsbech@gmail.com>
2020-01-15 15:51:50 +0100

    Færdiggjort draw of shuffle test i ChanceCardDeck-test

--- src/test/model/CardDeckTest.java
+++ src/test/model/CardDeckTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardDeckTest {
-public String[] testCardTexts = {"test","test","test","test","test","test","test","test","test","test"};
+public String[] testCardTexts = {"test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test"};
     @Test
     void swap() {
         assertTrue(true);
@@ -18,7 +18,7 @@ public String[] testCardTexts = {"test","test","test","test","test","test","test
         CardDeck deck = new CardDeck(testCardTexts);
         int[] occurences = new int[testCardTexts.length];
 
-        for (int i = 0; i < 1000 ; i++) {
+        for (int i = 0; i < 10000 ; i++) {
             //Shuffles deck
             deck.shuffle();
 
@@ -27,12 +27,38 @@ public String[] testCardTexts = {"test","test","test","test","test","test","test
         }
 
         for (int i = 0; i < testCardTexts.length; i++) {
-            assertTrue(occurences[i]<120 && occurences[i]> 80);
+            assertTrue(occurences[i]<240 && occurences[i]> 160);
         }
 
     }
 
     @Test
     void draw() {
+        int numberOfCards = testCardTexts.length;
+        int numberOfDraws = 10000;
+
+        //Tests that cards are put back in the bottom after being drawn
+        int[] occurences = new int[testCardTexts.length];
+        int occurence;
+
+        CardDeck deck = new CardDeck(testCardTexts);
+        for (int i = 0; i < numberOfDraws ; i++) {
+            //puts top card in bottom
+            deck.draw();
+
+            //The occurence is the tpo card
+            occurence= deck.getChanceCards()[0].getId();
+
+            //increments occurence
+            occurences[occurence]++;
+        }
+
+        for (int i = 0; i < testCardTexts.length; i++) {
+            assertTrue(occurences[i]<(numberOfDraws/numberOfCards)+1 );
+            assertTrue(occurences[i]> (numberOfDraws/numberOfCards)-1);
+        }
+
+
+
     }
 }
