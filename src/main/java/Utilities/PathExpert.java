1c63212b5a703bf0a5db4ae619cb8059e88f45bf
s195469 <s195469@student.dtu.dk>
2020-01-16 15:56:10 +0100

    Arabisk ؟ tilføjet på chancekort

--- src/main/java/Utilities/PathExpert.java
+++ src/main/java/Utilities/PathExpert.java
@@ -12,45 +12,13 @@ public class PathExpert {
 
     public static void setSprog(String sprog) {
         language = sprog;
-        namePath            = "language/" + language +"/fields/name";
-        fieldAttributesPath = "fieldAttributes";
-        descriptionMessagesPath   = "language/" + language +"/fields/description";
-        setupMessagesPath   = "language/" + language +"/messages/setupMessages";
-        turnMessagesPath   = "language/" + language +"/messages/turnMessages";
-        chanceCardPath   = "language/" + language +"/chanceCardTexts";
-        endMessagePath   = "language/" + language +"/messages/endGameMessages";
-    }
-
-    public static String getLanguage() {
-        return language;
-    }
-
-    public static String getNamePath() {
-        return namePath;
-    }
-
-    public static String getFieldAttributesPath() {
-        return fieldAttributesPath;
-    }
-
-    public static String getDescriptionMessagesPath() {
-        return descriptionMessagesPath;
-    }
-
-    public static String getSetupMessagesPath() {
-        return setupMessagesPath;
-    }
-
-    public static String getTurnMessagesPath() {
-        return turnMessagesPath;
-    }
-
-    public static String getChanceCardPath() {
-        return chanceCardPath;
-    }
-
-    public static String getEndMessagePath() {
-        return endMessagePath;
+        namePath                    = "language/" + language +"/fields/name";
+        fieldAttributesPath         = "fieldAttributes";
+        descriptionMessagesPath     = "language/" + language +"/fields/description";
+        setupMessagesPath           = "language/" + language +"/messages/setupMessages";
+        turnMessagesPath            = "language/" + language +"/messages/turnMessages";
+        chanceCardPath              = "language/" + language +"/chanceCardTexts";
+        endMessagePath              = "language/" + language +"/messages/endGameMessages";
     }
 }
 
