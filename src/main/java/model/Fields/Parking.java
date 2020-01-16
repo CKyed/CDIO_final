27ec83bb6cab7d68315792060d0445900dc470d5
peterrevsbech <peter.revsbech@gmail.com>
2020-01-16 13:14:32 +0100

    Har forhåbentlig fikset nogle problemer med bankrupcy, men har ikke haft tid til at teste endnu.

--- src/main/java/model/Fields/Parking.java
+++ src/main/java/model/Fields/Parking.java
@@ -1,7 +1,7 @@
 package model.Fields;
 
 public class Parking  extends Field {
-    private int group = 10;
+    private int group = 10; //TODO gør group nedarvet
     private String type = "parking";
     public Parking(String state) {
         super(state);
